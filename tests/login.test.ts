525b9d474f942c7a3736e0fe1e53ff6d369ac217
Dmitry Lankmiller <ditch725@gmail.com>
2024-05-24 15:01:00 +0300

    Test login: added failed LogIn test

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -4,18 +4,27 @@ import { MainPage } from '../pages/main.page';
 
 const BOT_LOGIN = 'technopol65';
 const BOT_PASSWORD = 'technopolisPassword';
+const BOT_WRONG_PASSWORD = 'qwerty';
 
 test.describe('Testing logging page', () => {
+	let loginPage: LoginPage;
 	test.beforeEach(async ({ page }) => {
+		loginPage = new LoginPage(page);
 		await page.goto('/');
 	});
 
 	test('Log in', async ({ page }) => {
-		let loginPage = new LoginPage(page);
 		await loginPage.setLogin(BOT_LOGIN);
 		await loginPage.setPassword(BOT_PASSWORD);
 		await loginPage.clickLogInBtn();
 		let mainPage = new MainPage(page);
-		await expect(mainPage.getProfileBtn()).toContainText(BOT_LOGIN);
+		await expect(mainPage.profileBtn).toContainText(BOT_LOGIN);
+	});
+
+	test('Failed log in', async ({ page }) => {
+		await loginPage.setLogin(BOT_LOGIN);
+		await loginPage.setPassword(BOT_WRONG_PASSWORD);
+		await loginPage.clickLogInBtn();
+		await expect(loginPage.failedLogInInfo).toBeVisible();
 	});
 });
